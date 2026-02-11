f8a408b375a39d9ba1cd64eb935d47b71808fb15
daland4ni <aeronroxas01@gmail.com>
2026-02-11 13:54:02 +0800

    added responsiveness to maintenance page; set as temporary default to github page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,30 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <div className="bg-gray-950 pt-30 pb-70 triangle-cut relative">
-        <div className="w-125 mx-25 p-3">
-          <h1 className="text-5xl ml-2 text-justified margin font-sans font-extrabold">
-            <span className="text-white">AERON</span> <span className="text-red-500">ROXAS</span>
-          </h1>
+      <div className="bg-black pt-30 pb-70 triangle-cut relative">
+        <div className="grid">
+          <div className="mx-25 p-3">
+            <h1 className="text-4xl text-justified margin font-bold">
+              <span className="text-yellow-200">Hello, I'm Aeron Jamil Roxas</span>
+            </h1>
+            <h1 className="text-7xl text-justified margin font-bold mt-7">
+              <span className="text-white">SOFTWARE DEVELOPER</span>
+            </h1>
+
+            <p className="text-gray-400 text-justify mt-7 w-150">
+              I am an upcoming BS Computer Engineering graduate from PUP, striving to contribute in software development roles.
+              In university, I had with projects that honed and challenged my programming skills that exposed me to trying out different languages and techniques.
+              I also have a leadership background in both academic and extra-curricular activities which made me a great communicator and problem solver.
+            </p>
+          </div>
+          <div className="bg-yellow-200 text-center w-35 ml-28 mt-6 rounded-xs px-1 py-2">
+            <span className="font-normal">Contact Me</span>
+          </div>
         </div>
       </div>
 
-      <div className="bg-purple-950 pt-50 pb-30 -mt-30">
-        <div className="bg-white
-        mx-25 p-3">
+      <div className="bg-gray-700 pt-70 pb-50 -mt-50">
+        <div className="bg-white mx-25 p-3">
           <h2 className="text-2xl text-bold">SOMETHING</h2>
         </div>
 
--- src/UM.tsx
+++ src/UM.tsx
@@ -3,17 +3,18 @@ import "./index.css";
 function Um() {
   return (
     <body className="bg-gray-800">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <div className="mt-60">
-        <h1 className="text-center font-extrabold text-7xl">
+        <h1 className="text-center font-extrabold text-3xl lg:text-6xl">
           <span className="text-white">
             UNDER </span><span className="text-pink-400">MAINTENANCE
           </span>
         </h1>
       </div>
 
-      <div className="mx-150 mt-10 text-center">
-      <p className="text-white font-light text-3xl">
-        Thank you for your interest in visiting Aeron Roxas' portfolio!<br />This site is currently being worked on at the moment.
+      <div className="w-screen mt-10 text-center">
+      <p className="text-white font-light lg:text-3xl sm:text-l">
+        Thank you for your interest in visiting my portfolio!<br />This site is currently being worked on at the moment.
       </p>
       </div>
 
